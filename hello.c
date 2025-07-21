6b7685ca93a88edcfb9afdf7d9c8c3ed0f4d622b
bnn16 <bogdan.nikolov4@Outlook.com>
2025-07-21 12:41:43 +0300

    ex-1.1

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { 
+        printf("hello bla \\c %s \n", "foo"); 
+        return 0;
+}
+
+
+// no new line or space between them duuh
+// int main() {
+//         printf("hello");
+//         printf("world");
+//         printf("\n");
+// }
